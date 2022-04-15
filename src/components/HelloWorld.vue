29ab865e532d5d97a26f67e55b905033ce4de178
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-04-15 14:46:45 +0300

    function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,26 +2,26 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div  class="grid ">
-      <div class="item current">0</div>
-      <div class="item">C</div>
-      <div class="item">+/-</div>
-      <div class="item">%</div>
-      <div class="item operator">/</div>
-      <div class="item">7</div>
-      <div class="item">8</div>
-      <div class="item">9</div>
-      <div class="item operator">X</div>
-      <div class="item">4</div>
-      <div class="item">5</div>
-      <div class="item">6</div>
-      <div class="item operator">-</div>
-      <div class="item">1</div>
-      <div class="item">2</div>
-      <div class="item">3</div>
-      <div class="item operator">+</div>
-      <dic class="zero item">0</dic>
-      <dic class="item">.</dic>
-      <dic class="item operator">=</dic>
+      <div class="item current">{{current || '0'}}</div>
+      <div @click="clear" class="item">C</div>
+      <div @click="calculation('+/-')" class="item">+/-</div>
+      <div @click="calculation('%')" class="item">%</div>
+      <div @click="calculation('/')" class="item operator">/</div>
+      <div @click="number(7)" class="item">7</div>
+      <div @click="number(8)" class="item">8</div>
+      <div @click="number(9)" class="item">9</div>
+      <div @click="calculation('X')" class="item operator">X</div>
+      <div @click="number(4)" class="item">4</div>
+      <div @click="number(5)" class="item">5</div>
+      <div @click="number(6)" class="item">6</div>
+      <div @click="calculation('-')" class="item operator">-</div>
+      <div @click="number(1)" class="item">1</div>
+      <div @click="number(2)" class="item">2</div>
+      <div @click="number(3)" class="item">3</div>
+      <div @click="calculation('+')" class="item operator">+</div>
+      <div @click="number(0)" class="zero item">0</div>
+      <div @click="dot" class="item">.</div>
+      <div @click="equals" class="item operator">=</div>
     </div>
 
   </div>
@@ -32,6 +32,65 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      current: '',
+      previous: null,
+      operator: null,
+      num1: '',
+      num2: '',
+    }
+  },
+  methods:{
+    number(num){
+      this.current += num
+    },
+    clear(){
+      this.current = ''
+    },
+    calculation(symbol){
+      switch (symbol){
+        case '-':
+          console.log('-')
+          this.operator = (a,b) => a - b
+          this.current = ''
+          break
+        case '+':
+          console.log('+')
+          return  this.operator = (a,b) => a + b
+          //break;
+        case '/':
+          console.log('/')
+          return  this.operator = (a,b) => a / b
+          //break;
+        case 'X':
+          console.log('x')
+          return  this.operator = (a,b) => a * b
+          //break;
+        case '+/-':
+          // if (a > 0 || a = 0){
+          //   return  '-' + a
+          // }else {
+          //   return  '+' + a
+          // }
+          break;
+        case '%':
+          console.log('%')
+          return  this.operator = (a) =>  a / 100
+        default:
+          console.log(`Sorry, we are out of }.`);
+      }
+    },
+    equals(){
+
+    },
+    dot(){
+
+    },
+    bind(){
+
+    }
   }
 }
 </script>
